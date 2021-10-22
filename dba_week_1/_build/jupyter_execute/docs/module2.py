199083a569841ec0768817c972a4a41a65f03d6b
Admin <admin@NOG-Filemaker-Server.local>
2021-10-22 13:53:03 +0200

    added html

--- dba_week_1/_build/jupyter_execute/docs/module2.py
+++ dba_week_1/_build/jupyter_execute/docs/module2.py
@@ -3,6 +3,8 @@
 
 # # Literature Search & Review September 2021
 # 
+# Interesting Jupyter URL: https://elc.github.io/posts/jupyter-publishing/
+# 
 # ```{figure} /_static/module2/LiteratureSearchExercise.png
 # :scale: 70%
 # ```
